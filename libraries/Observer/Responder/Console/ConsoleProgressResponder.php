e2827742d6fabd621afc9d64e82e5d13bc91a5aa
Christian Neff <christian.neff@gmail.com>
2014-11-28 14:45:26 +0100

    Added first classes of Observer namespace

---
+++ libraries/Observer/Responder/Console/ConsoleProgressResponder.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Seabreeze
+ * Copyright (C) 2014 IceFlame.net
+ *
+ * Permission to use, copy, modify, and/or distribute this software for
+ * any purpose with or without fee is hereby granted, provided that the
+ * above copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE
+ * FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY
+ * DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER
+ * IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
+ * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ * @package  FlameCore\Seabreeze
+ * @version  0.1-dev
+ * @link     http://www.flamecore.org
+ * @license  ISC License <http://opensource.org/licenses/ISC>
+ */
+
+namespace FlameCore\Seabreeze\Observer\Responder\Console;
+
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Helper\ProgressBar;
+
+/**
+ * This Responder writes a Progress Bar to the Console
+ *
+ * @author   Christian Neff <christian.neff@gmail.com>
+ */
+class ConsoleProgressResponder extends AbstractConsoleResponder
+{
+    protected $options;
+
+    protected $progress;
+
+    public function __construct(OutputInterface $output, array $options = [])
+    {
+        parent::__construct($output);
+
+        $this->options = $options;
+
+        $this->setListener('*.task.start',  [$this, 'onStart']);
+        $this->setListener('*.task.status', [$this, 'onStatus']);
+        $this->setListener('*.task.finish', [$this, 'onFinish']);
+    }
+
+    protected function onStart(array $data)
+    {
+        $format = isset($this->options['format']) ? $this->options['format'] : 'normal';
+
+        $maxSteps = isset($data['total']) ? (int) $data['total'] : 0;
+        unset($data['total']);
+
+        $progress = new ProgressBar($this->output, $maxSteps);
+        $progress->setFormat($format);
+        $progress->setEmptyBarCharacter(' ');
+        $progress->setProgressCharacter(':');
+
+        foreach ($data as $key => $value) {
+            $progress->setMessage($value, $key);
+        }
+
+        $progress->start();
+
+        $this->progress = $progress;
+    }
+
+    protected function onStatus(array $data)
+    {
+        if (!isset($data['progress']))
+            return;
+
+        $this->progress->setCurrent((int) $data['progress']);
+    }
+
+    protected function onFinish()
+    {
+        $this->progress->finish();
+
+        $this->output->write(PHP_EOL);
+    }
+}
