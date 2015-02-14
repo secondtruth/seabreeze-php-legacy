06d63981e85879a17d4c4b7d9fac2a79ad4e0f8c
Christian Neff <christian.neff@gmail.com>
2015-02-14 19:01:23 +0100

    Added binary source

---
+++ includes/autoload.php
@@ -0,0 +1,211 @@
+<?php
+/**
+ * Seabreeze
+ * Copyright (C) 2014 IceFlame.net
+ *
+ * Permission to use, copy, modify, and/or distribute this software for
+ * any purpose with or without fee is hereby granted, provided that the
+ * above copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE
+ * FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY
+ * DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER
+ * IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
+ * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ * @package  FlameCore\Seabreeze
+ * @version  0.1-dev
+ * @link     http://www.flamecore.org
+ * @license  ISC License <http://opensource.org/licenses/ISC>
+ */
+
+namespace FlameCore\Seabreeze;
+
+/**
+ * Loader for classes
+ *
+ * @author   Christian Neff <christian.neff@gmail.com>
+ */
+class ClassLoader
+{
+    /**
+     * List of source paths for namespace prefixes
+     *
+     * @var array
+     */
+    protected $sources = array();
+
+    /**
+     * Creates a ClassLoader instance.
+     *
+     * @param string $prefix The primary namespace prefix
+     * @param string $path The source path for the primary namespace
+     */
+    public function __construct($prefix, $path)
+    {
+        $this->addSource($prefix, $path);
+    }
+
+    /**
+     * Registers the ClassLoader as autoloader.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        spl_autoload_register([$this, 'loadClass']);
+    }
+
+    /**
+     * Checks whether a source path for the given namespace is defined.
+     *
+     * @param string $prefix The namespace prefix
+     * @return bool Returns TRUE if a source path for the given namespace is defined for the given namespace.
+     */
+    public function hasSource($prefix)
+    {
+        $prefix = $this->normalize($prefix);
+        return isset($this->sources[$prefix]);
+    }
+
+    /**
+     * Gets the source path for the given namespace.
+     *
+     * @param string $prefix The namespace prefix
+     * @return string Returns the source path of the namespace or NULL if no source is defined for the namespace.
+     */
+    public function getSource($prefix)
+    {
+        $prefix = $this->normalize($prefix);
+        return isset($this->sources[$prefix]) ? $this->sources[$prefix] : null;
+    }
+
+    /**
+     * Defines the source path for the given namespace.
+     *
+     * @param string $prefix The namespace prefix
+     * @param string $path The source path
+     * @param string $base Pattern of base path and filename (relative to source path).
+     */
+    public function addSource($prefix, $path, $base = 'libraries/*.php')
+    {
+        if ($this->hasSource($prefix)) {
+            return;
+        }
+
+        if (!is_dir($path)) {
+            throw new \DomainException(sprintf('The path "%s" does not exist.', $path));
+        }
+
+        if (strpos($base, '*') === false) {
+            throw new \InvalidArgumentException(sprintf('The base path does not contain a filename wildcard.', $prefix));
+        }
+
+        $prefix = $this->normalize($prefix);
+        $this->sources[$prefix] = $path.'/'.$base;
+    }
+
+    /**
+     * Loads the given class.
+     *
+     * @param string $name Name of the class to load
+     * @return bool Returns FALSE if the class could not be loaded, TRUE otherwise.
+     */
+    public function loadClass($name)
+    {
+        if ($classfile = $this->findFile($name)) {
+            require_once $classfile;
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Searches all source paths for a class file with given name.
+     *
+     * @param string $class Name of the class
+     * @return string|bool Returns the full filename of the found file. If no file is found FALSE is returned.
+     */
+    public function findFile($class)
+    {
+        $class = $this->normalize($class);
+
+        foreach ($this->sources as $prefix => $pattern) {
+            if (strpos($class, $prefix) === 0) {
+                $name_without_prefix = substr($class, strlen($prefix));
+                $file = str_replace('*', str_replace('\\', '/', $name_without_prefix), $pattern);
+
+                if (file_exists($file)) {
+                    return $file;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * List all classes of the given namespace.
+     *
+     * @param string $namespace The namespace
+     * @param bool $recursive Search classes recursively (Default: FALSE)
+     * @return array|bool Returns an array of classes in the namespace. If the namespace is not found FALSE is returned.
+     */
+    public function listClasses($namespace, $recursive = false)
+    {
+        $namespace = $this->normalize($namespace);
+
+        $found = false;
+        foreach ($this->sources as $prefix => $pattern) {
+            if (strpos($namespace, $prefix) === 0) {
+                $found = true;
+                break;
+            }
+        }
+
+        if (!$found) {
+            return false;
+        }
+
+        list($path_prefix, $path_suffix) = explode('*', $pattern);
+
+        $namespace_without_prefix = substr($namespace, strlen($prefix));
+        $path = $path_prefix . str_replace('\\', '/', $namespace_without_prefix);
+
+        $start = strlen($path_prefix);
+        $end = strlen($path_suffix) * -1;
+
+        $classes = array();
+        if ($recursive) {
+            $iterator = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($path, \FilesystemIterator::SKIP_DOTS | \FilesystemIterator::UNIX_PATHS));
+            foreach ($iterator as $file) {
+                if (substr($file->getBasename(), $end) == $path_suffix) {
+                    $classes[] = str_replace('/', '\\', substr($file->getPathName(), $start, $end));
+                }
+            }
+        } else {
+            $iterator = new \DirectoryIterator($path);
+            foreach ($iterator as $file) {
+                if ($file->isFile() && substr($file->getBasename(), $end) == $path_suffix) {
+                    $classes[] = substr($file->getBasename(), 0, $end);
+                }
+            }
+        }
+
+        return $classes;
+    }
+
+    /**
+     * Normalizes the given name.
+     *
+     * @param string $name The name to normalize
+     * @return string Returns the normalized name.
+     */
+    protected function normalize($name)
+    {
+        return trim($name, '\\');
+    }
+}
